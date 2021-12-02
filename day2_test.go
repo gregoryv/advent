e8315fcc982153f40530695547accc5b4763dde7
Gregory Vincic <g@7de.se>
2021-12-02 08:00:25 +0100

    Refactor

--- day2_test.go
+++ day2_test.go
@@ -12,42 +12,46 @@ import (
 
 func Example_Navigate_given() {
 	movements, _ := ioutil.ReadFile("testdata/2.given")
-	pos := Navigate(bytes.NewReader(movements))
+	pos := Navigate(bytes.NewReader(movements), WithoutAim)
 	fmt.Print(pos.h * pos.d)
 	// output: 150
 }
 
 func Example_Navigate_input() {
 	movements, _ := ioutil.ReadFile("testdata/2.input")
-	pos := Navigate(bytes.NewReader(movements))
+	pos := Navigate(bytes.NewReader(movements), WithoutAim)
 	fmt.Print(pos.h * pos.d)
 	// output: 1580000
 }
 
-func Navigate(r io.Reader) *Position {
+func Navigate(r io.Reader, calc func(*Position, string)) *Position {
 	s := bufio.NewScanner(r)
 	pos := &Position{}
 	for s.Scan() {
-		line := s.Text()
-		if line == "" {
-			continue
-		}
-		mov := strings.Split(line, " ")
-		v, _ := strconv.Atoi(mov[1])
-		switch mov[0] {
-		case "forward":
-			pos.h += v
-		case "down":
-			pos.d += v
-		case "up":
-			pos.d -= v
-		}
+		calc(pos, s.Text())
 	}
 
 	return pos
 }
 
+func WithoutAim(pos *Position, line string) {
+	if line == "" {
+		return
+	}
+	mov := strings.Split(line, " ")
+	v, _ := strconv.Atoi(mov[1])
+	switch mov[0] {
+	case "forward":
+		pos.h += v
+	case "down":
+		pos.d += v
+	case "up":
+		pos.d -= v
+	}
+}
+
 type Position struct {
-	h int
-	d int
+	aim int
+	h   int
+	d   int
 }
