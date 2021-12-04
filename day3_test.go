3430529b74ced35d0ecb16a90ffe2714b6b6e831
Gregory Vincic <g@7de.se>
2021-12-04 06:19:20 +0100

    Refactor

--- day3_test.go
+++ day3_test.go
@@ -1,57 +1,127 @@
 package advent
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
+	"os"
 	"strconv"
 )
 
-func Example_Radiation() {
-	Radiation("testdata/3.given", 5)
-	Radiation("testdata/3.input", 12)
+func Example_CalcRadiation() {
+	CalcRadiation("testdata/3.given", 5)
+	CalcRadiation("testdata/3.input", 12)
+
+	LifeSupportRating("testdata/3.given", 5)
+	LifeSupportRating("testdata/3.input", 12)
 
 	// output:
 	// 198
 	// 3009600
 }
 
-func Radiation(filename string, width int) {
-	data := loadLines(filename)
-
-	gamma := make([]byte, width)
-	epsilon := make([]byte, width)
-
-	for w := 0; w < width; w++ {
-		gamma[w] = '0'
-		epsilon[w] = '0'
-		var one, zero int
-
-		for _, line := range data {
-			if len(line) == 0 { // skip empty
-				continue
-			}
-			if line[w] == '1' {
-				one++
-			} else {
-				zero++
-			}
-		}
-		if one > zero {
-			gamma[w] = '1'
-		} else {
-			epsilon[w] = '1'
-		}
+func LifeSupportRating(filename string, width int) {
+	r, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
 	}
-
-	g, _ := strconv.ParseInt(string(gamma), 2, 64)
-	e, _ := strconv.ParseInt(string(epsilon), 2, 64)
-	fmt.Println(g * e)
+	defer r.Close()
+	lifeSupportRating(os.Stdout, r, width)
 }
 
-func loadLines(filename string) [][]byte {
-	data, err := ioutil.ReadFile(filename)
+func lifeSupportRating(w io.Writer, r io.Reader, width int) {
+
+}
+
+func NewRating() *Rating {
+	return &Rating{}
+}
+
+type Rating struct {
+	oxygen    []byte
+	scrubbing []byte
+}
+
+// ----------------------------------------
+
+func CalcRadiation(filename string, width int) {
+	data, _ := ioutil.ReadFile(filename)
+	CalcRadiationTo(os.Stdout, bytes.NewReader(data), width)
+}
+
+func CalcRadiationTo(w io.Writer, r io.Reader, width int) {
+	rad := NewRadiation(width)
+	rad.Parse(r)
+	fmt.Println(rad.Gamma() * rad.Epsilon())
+}
+
+func NewRadiation(width int) *Radiation {
+	return &Radiation{
+		one:  make([]int, width),
+		zero: make([]int, width),
+
+		gamma:   bytes.Repeat([]byte("0"), width),
+		epsilon: bytes.Repeat([]byte("0"), width),
+	}
+}
+
+type Radiation struct {
+	one  []int
+	zero []int
+
+	gamma   []byte
+	epsilon []byte
+}
+
+func (me *Radiation) Parse(r io.Reader) {
+	s := bufio.NewScanner(r)
+	for s.Scan() {
+		me.Write(s.Bytes())
+	}
+}
+
+func (me *Radiation) Write(p []byte) (int, error) {
+	if len(p) == 0 { // skip empty
+		return 0, nil
+	}
+	for i := 0; i < len(me.gamma); i++ {
+		if p[i] == '1' {
+			me.one[i]++
+		} else {
+			me.zero[i]++
+		}
+	}
+	return len(p), nil
+}
+
+func (me *Radiation) Update() {
+	for i := 0; i < len(me.gamma); i++ {
+		if me.one[i] > me.zero[i] {
+			me.gamma[i] = '1'
+		} else {
+			me.epsilon[i] = '1'
+		}
+	}
+}
+
+func (me *Radiation) Gamma() int64 {
+	me.Update()
+	v, _ := strconv.ParseInt(string(me.gamma), 2, 64)
+	return v
+}
+func (me *Radiation) Epsilon() int64 {
+	me.Update()
+	v, _ := strconv.ParseInt(string(me.epsilon), 2, 64)
+	return v
+}
+
+// ----------------------------------------
+
+func loadLines(r io.Reader) [][]byte {
+	data, err := io.ReadAll(r)
 	if err != nil {
 		log.Fatal(err)
 	}
