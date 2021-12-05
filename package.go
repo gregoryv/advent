d355ede57e34f845d9055084e6d714ea91fb0f3d
Gregory Vincic <g@7de.se>
2021-12-05 08:00:19 +0100

    Add logPrinter debug

--- package.go
+++ package.go
@@ -11,3 +11,16 @@ func shouldNot(err error) {
 
 // Used by shouldNot
 var fatalHandler func(...interface{}) = log.Fatal
+
+// useful to debug things during testing, replace it with testing.T
+var debug logPrinter = &discard{}
+
+type discard struct{}
+
+func (*discard) Log(...interface{})          {}
+func (*discard) Logf(string, ...interface{}) {}
+
+type logPrinter interface {
+	Log(v ...interface{})
+	Logf(format string, v ...interface{})
+}
